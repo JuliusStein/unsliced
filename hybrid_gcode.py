1db382088c0b31e385d30689f34fdd4c8c65c41f
JuliusStein <juliusastein@gmail.com>
2022-12-13 13:15:51 -0500

    presentation ready

--- hybrid_gcode.py
+++ hybrid_gcode.py
@@ -33,9 +33,12 @@ def makeCollumn(x, y, z_start, z_end, e_base, eject_rate, height, move_speed):
 
     return lines
 
+# make a spiral of a given diameter at a given height
+# returns a list of gcode lines
 def makeSpiral(x, y, z_start, z_end, r, e_base, eject_rate, height, move_speed):
     # x, y are the coordinates of the center of the collumn
     # z_start, z_end are the start and end z values of the collumn
+    # r is the radius of the spiral
     # e_base is the starting extrusion value
     # eject_rate is the rate of the print head's fillament extrusion
     # height is the height of the collumn in layers
@@ -52,17 +55,17 @@ def makeSpiral(x, y, z_start, z_end, r, e_base, eject_rate, height, move_speed):
     # make a list of lines for each layer
     lines = []
     lines.append("G1 X{} Y{} Z{} F{}\n".format(round(x+3, 2), round(y+3, 2), round(z_start+100, 2), 2400))
-    lines.append("G1 X{} Y{} Z{} E{} F{}\n".format(round(x, 2), round(y, 2), round(z_start+3, 2), e_base, 2400))
+    lines.append("G1 X{} Y{} Z{} E{} F{}\n".format(round(x, 2), round(y, 2), round(z_start+3, 2), round(e_base,2), 2400))
     lines.append("G4 P500\n")
     
     for i, z_val in enumerate(z_values):
         t = 0.1*i*pi
         if i==0:
-            lines.append("G1 X{} Y{} Z{} E{} F{}\n".format(x + round(r*cos(t), 2), y+ round(r*sin(t), 2), z_val, round(e_base, 2), move_speed))
-        elif t<2*pi:
-            lines.append("G1 X{} Y{} E{}\n".format(x + round(r*cos(t), 2), y+ round(r*sin(t), 2), round(e_base + eject_rate*i,2)))
+            lines.append("G1 X{} Y{} Z{} E{} F{}\n".format(round(x + r*cos(t), 2), round(y+ r*sin(t), 2), z_val, round(e_base, 2), move_speed))
+        elif t<2*pi or t>z_end - .2*pi:
+            lines.append("G1 X{} Y{} E{}\n".format(round(x + r*cos(t), 2), round(y+ r*sin(t), 2), round(e_base + eject_rate*i,2)))
         else:
-            lines.append("G1 X{} Y{} Z{} E{}\n".format(x + round(r*cos(t), 2), y+ round(r*sin(t), 2), round(z_val-2*pi,2), round(e_base + eject_rate*i,2)))
+            lines.append("G1 X{} Y{} Z{} E{}\n".format(round(x + r*cos(t), 2), round(y+ r*sin(t), 2), round(z_val-2*pi,2), round(e_base + eject_rate*i,2)))
     
     lines.append("G4 P500\n")
 
@@ -127,22 +130,36 @@ e_stop = getEVal(e_stop_lines, True)
 e_range = e_stop - e_start
 
 #Inject gcode below
-speed = 30
-numCollumns = 4
-eject_rate = round((e_range/injectLayers)/numCollumns,2)
-print("eject_rate: ", eject_rate)
 
 newLines += lines[:layer_lines[startInject]]
 
-e_start += 5
-newLines += makeCollumn(0, -8.2, 3.4, 28.65, e_start, eject_rate, injectLayers, speed)
-newLines += makeCollumn(-8.2, 0, 3.4, 28.65, e_start + (0.25*e_range), eject_rate, injectLayers, speed)
-newLines += makeCollumn(0, 8.2, 3.3, 28.65, e_start + (0.5*e_range), eject_rate, injectLayers, speed)
-newLines += makeCollumn(8.2, 0, 3.3, 28.65, e_start + (0.75*e_range), eject_rate, injectLayers, speed)
+# speed = 30
+# e_start += 5
+# numCollumns = 4
+# eject_rate = round((e_range/injectLayers)/numCollumns,2)
+# newLines += makeCollumn(0, -8.2, 3.4, 28.6, e_start, eject_rate, injectLayers, speed)
+# newLines += makeCollumn(-8.2, 0, 3.4, 28.6, e_start + (0.25*e_range), eject_rate, injectLayers, speed)
+# newLines += makeCollumn(0, 8.2, 3.3, 28.6, e_start + (0.5*e_range), eject_rate, injectLayers, speed)
+# newLines += makeCollumn(8.2, 0, 3.3, 28.6, e_start + (0.75*e_range), eject_rate, injectLayers, speed)
+
+speed=80
+numSpirals = 4
+eject_rate = round((e_range/injectLayers)/numSpirals ,2)*0.25
+newLines += makeSpiral(0, -7.2, 3.5, 39.6, 1.4, e_start, eject_rate, injectLayers, speed)
+newLines += makeSpiral(-7.2, 0, 3.5, 39.6, 1.4, e_start + (0.25*e_range*.25), eject_rate, injectLayers, speed)
+newLines += makeSpiral(0, 7.2, 3.5, 39.6, 1.4, e_start + (0.5*e_range*.25), eject_rate, injectLayers, speed)
+newLines += makeSpiral(7.2, 0, 3.5, 39.6, 1.4, e_start + (0.75*e_range*.25), eject_rate, injectLayers, speed)
+
+# speed=40
+# numCollumns = 1
+# eject_rate = round((e_range/injectLayers)/numCollumns,2)*0.25
+# print("eject_rate: ", eject_rate)
+
+newLines += makeSpiral(0, -7.2, 3.5, 39.6, 5, e_start, eject_rate, injectLayers, speed)
 
 newLines.append(f"G92 E{e_stop}\n")
 newLines += lines[layer_lines[endInject]:]
 
 # write new gcode file
-with open("TestObjectA_hybrid_v2.g", "w") as f:
+with open("TestObjectA_hybrid_v4.g", "w") as f:
     f.writelines(newLines)
